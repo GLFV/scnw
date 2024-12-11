403baec690dae0c32f7726e730a9f8d1ce813a19
GLFV <okolesnik753@gmail.com>
2024-12-11 19:48:43 +0100

    Refactor: moved login logic from user route to auth route

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const UserRoutes = require('./routes/userRoutes');
+const AuthRouter = require('./routes/Auth/Auth.js');
 const cookieParser = require('cookie-parser');
-const jwt = require('jsonwebtoken');
-const { generateAccessToken } = require('./routes/Auth/tokens.js');
 
 const app = express();
 
@@ -11,31 +9,10 @@ dotenv.config();
 
 app.use(cookieParser());
 app.use(express.json());
-app.use('/user', UserRoutes);
+app.use('/auth', AuthRouter);
 
 app.get('/', (req, res) => {
   res.send('hello world');
-});
-
-app.post('/refresh', (req, res) => {
-  const refreshToken = req.cookies.refreshToken;
-  console.log(refreshToken);
-  if (!refreshToken) {
-    return res.status(401).json({ message: 'Refresh token is required' });
-  }
-
-  jwt.verify(refreshToken, process.env.JWT_REFRESH, (err, user) => {
-    if (err) {
-      return res.status(403).json({ message: 'Invalid refresh token' });
-    }
-
-    const newAccessToken = generateAccessToken({
-      id: user.id,
-      email: user.email,
-    });
-
-    res.json({ accessToken: newAccessToken });
-  });
 });
 
 app.listen(3000, () => {
